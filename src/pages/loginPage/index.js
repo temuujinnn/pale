fd7591639a32b3c314040eab0dd59b370727488d
tserentogtoh <71225024+tserentogtoh@users.noreply.github.com>
2021-03-20 16:58:25 +0800

    naba

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import css from "./style.module.css";
 import Background from "../../img/login.jpg"
-
+import Logo from "../../component/Logo"
 const LoginPage = () => {
   return (
     <div className = {css.all}>
       <div className={css.Container}>
         <img  className = { css.img} src = {Background} alt ="zurag"/>
-        <div className={css.logo}>logo</div>
+        <div className={css.logo}>
+          <Logo/>
+          </div>
         <div className = {css.form}>
           <h1>Нэвтрэх</h1>
           <b>Таны нэр:</b>
