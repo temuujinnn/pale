4f6353860ee62f9b023a097141ef4155a66e6ccb
Naba0224 <nblikeit@gmail.com>
2021-03-20 15:16:11 +0800

    1'

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import css from "./style.module.css";
 const LoginPage = () => {
-<<<<<<< HEAD
   return (
     <div className={css.Container}>
       <div className={css.input}>INPUT</div>
     </div>
   );
-=======
-  return <div>
-
-    <div>
-      
-    </div>
-
-  </div>;
->>>>>>> 79d59a1d570efa7444c5c60d6b42a8f2d707568b
 };
 export default LoginPage;
