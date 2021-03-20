773fc8f5cfd5ec382c358b040a5ec2f153205bfb
temuujinnn <temvkaa@gmail.com>
2021-03-21 01:53:36 +0800

    temkaaaaa line border

--- src/component/Footer/index.js
+++ src/component/Footer/index.js
@@ -3,7 +3,7 @@ import css from "./style.module.css";
 const Footer = () => {
   return (
     <div className={css.Container}>
-      <div className={css.line}></div>
+      {/* <div className={css.line}></div> */}
       <div className={css.text}>
         ПАЛЕОНТОЛОГИЙН ХҮРЭЭЛЭН <strong>© 2021</strong>. Бүх эрх хуулиар
         хамгаалагдсан.
