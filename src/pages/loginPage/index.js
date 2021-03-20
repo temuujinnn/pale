4fde6ae8a1d1f0c4e309b611623df35131078220
tserentogtoh <71225024+tserentogtoh@users.noreply.github.com>
2021-03-20 16:48:29 +0800

    tseke

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import css from "./style.module.css";
+import Background from "../../img/login.jpg"
+
 const LoginPage = () => {
   return (
-    <div className={css.Container}>
-      <div className={css.input}>INPUT</div>
+    <div className = {css.all}>
+      <div className={css.Container}>
+        <img  className = { css.img} src = {Background} alt ="zurag"/>
+        <div className={css.logo}>logo</div>
+        <div className = {css.form}>
+          <b>Таны нэр:</b>
+            <input type="text" className={css.input} placeholder="Та нэвтрэх нэрээ бичнэ үү" />
+           <b>Нууц үг:</b>  
+            <input type="text" className={css.input} placeholder="Та нууц үгээ оруулна уу"/>
+          </div>
+      </div>
     </div>
   );
 };
