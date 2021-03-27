553a80e27f7467e0f5d8a6b77c4a4d7db675de39
tserentogtoh <71225024+tserentogtoh@users.noreply.github.com>
2021-03-27 11:30:12 +0800

    j

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -2,23 +2,34 @@ import React from "react";
 import css from "./style.module.css";
 import Background from "../../img/login.jpg"
 import Logo from "../../component/Logo"
+import { Link } from "react-router-dom";
 const LoginPage = () => {
   return (
     <div className = {css.all}>
-      <div className={css.Container}>
-        <img  className = { css.img} src = {Background} alt ="zurag"/>
-         <div className={css.logo}>
-          <Logo/>
+      <img  className = { css.img} src = {Background} alt ="zurag"/>
+      <div className = {css.naba}>
+          <div className={css.Container}>
+            <div className = {css.section}>
+                  <div className={css.logo}>
+                  <Logo/>
+                  </div>
+                  <div className = {css.form}>
+                    <p>Нэвтрэх</p>
+                    <b>Таны нэр:</b>
+                    <input type="text" className={css.input} placeholder="Та нэвтрэх нэрээ бичнэ үү" />
+                    <b>Нууц үг:</b>  
+                    <input type="text" className={css.input} placeholder="Та нууц үгээ оруулна уу"/>
+                    <label className={css.checkbox}>
+                    <input type="checkbox"  />
+                    Намайг сана
+                    </label>
+                    <Link className={css.Link} to="/"> Нэвтрэх</Link>
+                  </div>
+            </div>
+            <p className = {css.p}>Хэрвээ та анх удаа нэвтэрч байгаа бол мэдээллийн ажилтантай холбогдож бүртгүүлнэ үү.</p> 
           </div>
-        <div className = {css.form}>
-          <p>Нэвтрэх</p>
-          <b>Таны нэр:</b>
-            <input type="text" className={css.input} placeholder="Та нэвтрэх нэрээ бичнэ үү" />
-           <b>Нууц үг:</b>  
-            <input type="text" className={css.input} placeholder="Та нууц үгээ оруулна уу"/>
-          </div>
-          <p className = {css.p}>Хэрвээ та анх удаа нэвтэрч байгаа бол мэдээллийн ажилтантай холбогдож бүртгүүлнэ үү.</p>
       </div>
+    
     </div>
   );
 };
