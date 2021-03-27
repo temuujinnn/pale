a71e4106683f83c50cb2db84ad6a0307ef130647
temuujinnn <temvkaa@gmail.com>
2021-03-27 18:48:32 +0800

    temkkka addmodal

--- src/pages/homePage/index.js
+++ src/pages/homePage/index.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useState } from "react";
 import Navbar from "../../component/Navbar";
 import Main from "../../component/main";
 import css from "./style.module.css";
@@ -8,43 +9,71 @@ import LastAdded from "../../component/LastAdded";
 import Contact from "../../component/Contact";
 import Footer from "../../component/Footer";
 import { Link } from "react-router-dom";
+import Modal from "react-modal";
+import AddModal from "../../component/addModal";
+const customStyles = {
+  content: {
+    left: "50%",
+    top: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "40%",
+    height: "80%",
+  },
+};
 
-const HomePage = () => {
+const HomePage = (props) => {
+  const [display, setDisplay] = useState(true);
+  const close = () => {
+    setDisplay(false);
+  };
   return (
-    <div className={css.Container}>
-      <div className={css.main}>
-        <Main />
-      </div>
-      <div className={css.Nav}>
-        <Navbar />
-      </div>
+    <Fragment>
+      <div className={css.Container}>
+        <div className={css.main}>
+          <Main />
+        </div>
+        <div className={css.Nav}>
+          <Navbar />
+        </div>
 
-      <div className={css.layer}>
-        <p className={css.middleText}>
-          Монгол улсын шинжлэх ухааны академи <br /> Палеонтологийн хүрээлэн
-        </p>
-        <p className={css.san}>бүртгэлийн нэгдсэн сан</p>
-        <Link to="loginpage">
-          <button className={css.Bttn}>Нэвтрэх</button>
-        </Link>
-      </div>
+        <div className={css.layer}>
+          <p className={css.middleText}>
+            Монгол улсын шинжлэх ухааны академи <br /> Палеонтологийн хүрээлэн
+          </p>
+          <p className={css.san}>бүртгэлийн нэгдсэн сан</p>
+          <Link to="loginpage">
+            <button className={css.Bttn}>Нэвтрэх</button>
+          </Link>
+        </div>
 
-      <div className={css.Category}>
-        <Category />
+        <div className={css.Category}>
+          <Category />
+        </div>
+        <div className={css.BodyText}>
+          <BodyText />
+        </div>
+        <div className={css.LastAdded}>
+          <LastAdded />
+          <Modal
+            isOpen={display}
+            shouldCloseOnOverlayClick={false}
+            onRequestClose={() => setDisplay(false)}
+            style={customStyles}
+          >
+            <AddModal close={close} />
+          </Modal>
+        </div>
+        <div className={css.Contact}>
+          <Contact />
+        </div>
+        <div className={css.footer}>
+          <Footer />
+        </div>
       </div>
-      <div className={css.BodyText}>
-        <BodyText />
-      </div>
-      <div className={css.LastAdded}>
-        <LastAdded />
-      </div>
-      <div className={css.Contact}>
-        <Contact />
-      </div>
-      <div className={css.footer}>
-        <Footer />
-      </div>
-    </div>
+    </Fragment>
   );
 };
 export default HomePage;
