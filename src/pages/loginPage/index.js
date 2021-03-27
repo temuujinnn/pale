cb0ee1acf41daea23ecc40e5de70b00c7245932f
tserentogtoh <71225024+tserentogtoh@users.noreply.github.com>
2021-03-27 11:44:25 +0800

    hj

--- src/pages/loginPage/index.js
+++ src/pages/loginPage/index.js
@@ -1,38 +1,6 @@
 import React, { Fragment } from "react";
 import css from "./style.module.css";
-<<<<<<< HEAD
-import Background from "../../img/login.jpg"
-import Logo from "../../component/Logo"
-import { Link } from "react-router-dom";
-const LoginPage = () => {
-  return (
-    <div className = {css.all}>
-      <img  className = { css.img} src = {Background} alt ="zurag"/>
-      <div className = {css.naba}>
-          <div className={css.Container}>
-            <div className = {css.section}>
-                  <div className={css.logo}>
-                  <Logo/>
-                  </div>
-                  <div className = {css.form}>
-                    <p>Нэвтрэх</p>
-                    <b>Таны нэр:</b>
-                    <input type="text" className={css.input} placeholder="Та нэвтрэх нэрээ бичнэ үү" />
-                    <b>Нууц үг:</b>  
-                    <input type="text" className={css.input} placeholder="Та нууц үгээ оруулна уу"/>
-                    <label className={css.checkbox}>
-                    <input type="checkbox"  />
-                    Намайг сана
-                    </label>
-                    <Link className={css.Link} to="/"> Нэвтрэх</Link>
-                  </div>
-            </div>
-            <p className = {css.p}>Хэрвээ та анх удаа нэвтэрч байгаа бол мэдээллийн ажилтантай холбогдож бүртгүүлнэ үү.</p> 
-          </div>
-      </div>
-    
-    </div>
-=======
+
 import Img from "../../img/login.jpg";
 import Logo from "../../component/Logo";
 import LoginInput from "../../component/loginInput";
@@ -88,7 +56,7 @@ const LoginPage = () => {
         ПАЛЕОНТОЛОГИЙН ХҮРЭЭЛЭН © 2021. Бүх эрх хуулиар хамгаалагдсан.
       </div>
     </>
->>>>>>> 80fca2820348758b72b2103bf53a81a2965370c9
+
   );
 };
 export default LoginPage;
