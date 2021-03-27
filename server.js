f8d8dbecf43680adabed6e8958bb5caab4d4ceba
temuujinnn <temvkaa@gmail.com>
2021-03-27 22:42:37 +0800

    Updates

--- server.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req);
-  res.write("<h1>sain banuu 500 servert tawtai moril</h1>");
-  res.end();
-});
-server.listen(5000, () => {
-  console.log("server aslaa");
-});
--- src/pages/signupPage/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const SignUpPage = () => {
-  return <div>signUpPage</div>;
-};
-export default SignUpPage;
