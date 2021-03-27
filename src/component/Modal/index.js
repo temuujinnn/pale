328dfb9062f9022512d1e2f01cda7109f02499d3
tserentogtoh <71225024+tserentogtoh@users.noreply.github.com>
2021-03-27 19:24:23 +0800

    tseke member add

--- src/component/Modal/index.js
+++ src/component/Modal/index.js
@@ -2,61 +2,72 @@ import React from "react";
 import css from "./style.module.css";
 const Modal = (props) => {
   return (
-    <div className={css.Container}>
+    
 
       <div className={css.ModalBackdrop}>
         <div className={css.ModalBox}>
-        <div className={css.inputPass}>
+        <div className={css.inputlName}>
                 <strong className={css.label}>Овог</strong>
                 <input
                   className={css.input}
                   type="text"
-                  placeholder="   Та нэвтрэх нууц үгээ оруулна уу"
+                  placeholder=" Хэрэглэгчийн овог"
                 />
         </div>  
-        <div className={css.inputName}>
+        <div className={css.inputfName}>
                 <strong className={css.label}>Нэр </strong>
                 <input
                   className={css.input}
                   type="text"
-                  placeholder="   Та нэвтрэх ID-аа бичнэ үү"
+                  placeholder=" Хэрэглэгчийн нэр"
                 />
         </div>  
         <div className={css.inputName}>
-                <strong className={css.label}>Таны ID </strong>
+                <strong className={css.label}>Хэрэглэгчийн ID </strong>
                 <input
                   className={css.input}
                   type="text"
-                  placeholder=" Та нэвтрэх ID-аа бичнэ үү"
-                />
-        </div>    
-        <div className={css.inputName}>
-                <strong className={css.label}>Таны ID </strong>
-                <input
-                  className={css.input}
-                  type="text"
-                  placeholder=" Та нэвтрэх ID-аа бичнэ үү"
+                  placeholder=" Хэрэглэгчийн ID оруулнуу"
                 />
               </div>
+        <div className={css.inputpass}>
+                <strong className={css.label}>Нууц үг </strong>
+                <input
+                  className={css.input}
+                  type="text"
+                  placeholder=" Нууц үг"
+                />
+        </div>    
+        <div className={css.inputpass}>
+                <strong className={css.label}>Нууц үг давтах </strong>
+                <input
+                  className={css.input}
+                  type="text"
+                  placeholder=" Нууц үг "
+                />
+        </div>    
+        
         <div className = {css.inputSelect}>
-          <select name="position" id="position" >
-          <option value="Admin">Админ</option>
-          <option value="researcher">Судлаач</option>
-          <option value="editor">Эрхлэгч</option>
-          <option value="sanhomrogch">Сан хөтрөгч</option>
+          <strong className ={css.label}>Хандалтын төрөл</strong>
+          <select className ={css.select}  name="position" id="position" >
+          <option className ={css.option} value="Admin">Админ</option>
+          <option className ={css.option} value="researcher">Судлаач</option>
+          <option className ={css.option} value="editor">Эрхлэгч</option>
+          <option className ={css.option} value="sanhomrogch">Сан хөтрөгч</option>
           </select>
         </div>
-          <button type="submit" onClick={props.close}>
-            Буцах
-          </button>
+        <button className={css.button} type="submit" onClick={props.close}>
+          Буцах
+        </button>
+        <button className={css.button} type="submit" onClick={props.close}>
+          Хадгалах
+        </button>
+
+
         </div>
       </div>
 
-      <button type="submit" onClick={props.close}>
-        Буцах
-      </button>
-
-    </div>
+    
   );
 };
 export default Modal;
